3f05033a6d00079123e7d46d950f0281a1f05de2
Gary Taylor <ext.gary.taylor@farfetch.com>
2021-09-06 19:23:31 +0100

    Initial commit

---
+++ WordChallenge_Sln/WordChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
